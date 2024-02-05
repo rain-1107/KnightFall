fba070ee1810695285c0e980f4d99b2e46e7efa9
triggered_idiot <82025800+triggered0idiot@users.noreply.github.com>
2024-02-05 18:57:03 +0000

    add dragging to lvl editor
    too much to list busy rn sry

--- Level editor/main.py
+++ Level editor/main.py
@@ -2,6 +2,9 @@ import KTFL
 import pygame
 import json
 
+from KTFL.util import Vector2
+from KTFL.gui import draw_grid
+
 screen = KTFL.display.Display(size=(1600, 900))
 ui_cam = KTFL.display.Camera(size=(1600, 900))
 level_cam = KTFL.display.Camera(size=(1300, 900), position=(300, 0), display_size=(1300, 900))
@@ -14,8 +17,12 @@ inputs = {}
 current_level = KTFL.level.Level("levels/default.json")
 current_level.load()
 
+selected_sprite_text_surface_index = None # i know this sucks but like idc its better than what i was going to do
+selected_sprite = None
+selected_sprite_offset = None
 
 def create_menu():
+    global selected_sprite_text_surface_index # this is so fucking dumb
     text_surfaces.append([KTFL.gui.get_text_surf("Load level from file"), (20, 35)])
     text_surfaces.append([KTFL.gui.get_text_surf("Save level"), (20, 95)])
     buttons["load"] = KTFL.gui.Button((75, 30), (120, 50), "bin/images/button0.png", "bin/images/button1.png",
@@ -44,6 +51,10 @@ def create_menu():
 
     inputs["id"] = KTFL.gui.TextInput((75, 30), (100, 450), "bin/images/input.png", text="")
     text_surfaces.append([KTFL.gui.get_text_surf("ID"), (20, 455)])
+    
+    inputs["gridsnapx"] = KTFL.gui.TextInput((75, 30), (100, 500), "bin/images/input.png", text="")
+    inputs["gridsnapy"] = KTFL.gui.TextInput((75, 30), (200, 500), "bin/images/input.png", text="")
+    text_surfaces.append([KTFL.gui.get_text_surf("GRID SNAP"), (10, 505)])
 
     buttons["create"] = KTFL.gui.Button((75, 30), (200, 350), "bin/images/button0.png", "bin/images/button1.png",
                                       function=create_sprite, text="Create")
@@ -51,6 +62,9 @@ def create_menu():
                                         function=edit_sprite, text="Edit")
     buttons["delete"] = KTFL.gui.Button((75, 30), (200, 450), "bin/images/button0.png", "bin/images/button1.png",
                                         function=delete_sprite, text="Delete")
+
+    text_surfaces.append([KTFL.gui.get_text_surf("???"), (10, 800)]) #what sprite we selecting (TODO : ryan should add sprite names)
+    selected_sprite_text_surface_index = len(text_surfaces) - 1
 
 
 def move_cam_left():
@@ -102,6 +116,7 @@ def update_level_cam():
 
 
 def update_menu():
+    global selected_sprite,selected_sprite_offset # argh
     pygame.draw.rect(ui_cam.surface, (255, 255, 255), (0, 0, 300, 900))
     pygame.draw.rect(ui_cam.surface, (0, 0, 0), (0, 0, 300, 900), width=3)
     for surf in text_surfaces:
@@ -111,6 +126,20 @@ def update_menu():
     for key in inputs:
         inputs[key].update(ui_cam)
 
+    fixed_mouse_pos = screen.control.mouse_pos()
+    fixed_mouse_pos = (fixed_mouse_pos[0] - level_cam.position.list[0],fixed_mouse_pos[1] - level_cam.position.list[1])
+
+    text_surfaces[selected_sprite_text_surface_index][0] = KTFL.gui.get_text_surf("???")
+    for sprite in current_level.sprites:
+        if sprite.is_point_in_sprite(fixed_mouse_pos):
+            text_surfaces[selected_sprite_text_surface_index][0] = KTFL.gui.get_text_surf(str(sprite.id))
+            if screen.control.mouse_button(1) == "down":
+                selected_sprite = sprite
+                selected_sprite_offset = Vector2(fixed_mouse_pos[0],fixed_mouse_pos[1]) - sprite.position
+        if screen.control.mouse_button(1) == "held" and selected_sprite:
+            selected_sprite.set_position(Vector2(fixed_mouse_pos[0] - selected_sprite_offset.x, fixed_mouse_pos[1] - selected_sprite_offset.y))
+
+    #draw_grid(level_cam)
 
 def create_sprite():
     print("new sprite")
@@ -156,5 +185,5 @@ update_level_cam()
 
 while True:
     update_menu()
+    update_level_cam()
     screen.update()
-
